ee81a9baeb8ca36840948996e8be82f23578232e
sidk44 <siddharth.kms44@gmail.com>
2024-08-06 22:15:09 +0530

    Vercel6

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Simple-Movie-Search-with-API/',
-  plugins: [react()],
-})
-
 // export default defineConfig({
 //   base: '/Simple-Movie-Search-with-API/',
 //   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['@suprsend/react-inbox']
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ['@suprsend/react-inbox']
-//     }
-//   }
-// });
+// })
+
+export default defineConfig({
+  base: '/Simple-Movie-Search-with-API/',
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@suprsend/react-inbox']
+  },
+  build: {
+    rollupOptions: {
+      external: ['@suprsend/react-inbox']
+    }
+  }
+});
