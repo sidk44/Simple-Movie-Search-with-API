cd5311911b2a4e0ed54e82993223b32752aac75c
sidk44 <siddharth.kms44@gmail.com>
2024-08-06 19:36:35 +0530

    notifications-vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['@suprsend/react-inbox']
+  },
+  build: {
+    rollupOptions: {
+      external: ['@suprsend/react-inbox']
+    }
+  }
+});
